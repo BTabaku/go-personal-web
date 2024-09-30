2d71f52d7134197dbecd18bd663f72ed8be09db1
BTabaku <baftit40@gmail.com>
2024-09-30 16:08:33 +0200

    Remove unnecessary files and update server package in main.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-personal-web
+
+go 1.22.5
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,24 @@
 package config
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Config struct {
+	Port string `json:"port"`
+}
+
+func LoadConfig(filename string) (*Config, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	config := &Config{}
+	if err := json.NewDecoder(file).Decode(config); err != nil {
+		return nil, err
+	}
+	return config, nil
+}
